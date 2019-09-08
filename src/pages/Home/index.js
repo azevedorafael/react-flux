a71d45b5b975982b717c1d4f5572ab038f8005d4
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-09-08 15:30:12 -0300

    Add Home page styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,89 @@
 import React from 'react';
+import {MdAddShoppingCart} from  'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <ProductList>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://imgcentauro-a.akamaihd.net/180x180/93457831.jpg" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
